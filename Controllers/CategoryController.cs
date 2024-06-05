bca381f2d0b13547f3159d847d10876ac817b196
jairojunior955 <jairojunior955@gmail.com>
2024-06-05 16:10:38 -0400

    feat(CategoryController): Category Controller Completed

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using MockApi.Services;
+using System.Threading.Tasks;
+
+namespace MockApi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CategoryService _categoryService;
+
+        public CategoryController(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+            if (category  ==  null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}

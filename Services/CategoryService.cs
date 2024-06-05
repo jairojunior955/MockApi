fa0e6d8ba4ee3d68f46ff83a9a2f127b8fd905c3
jairojunior955 <jairojunior955@gmail.com>
2024-06-05 17:54:05 -0400

    feat(CategoryService): CHanging to .env file

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -5,23 +5,31 @@ using MockApi.Models;
 
 namespace MockApi.Services
 {
+    // CategoryServices.cs
     public class CategoryService
     {
         private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl;
 
         public CategoryService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+            _apiBaseUrl = Environment.GetEnvironmentVariable("API_BASE_URL");
+            if (string.IsNullOrEmpty(_apiBaseUrl))
+            {
+                throw new InvalidOperationException("API_BASE_URL environment variable is not set.");
+            }
         }
 
         public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
         {
-            return await _httpClient.GetFromJsonAsync<IEnumerable<Category>>("*");
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Category>>(_apiBaseUrl);
         }
 
         public async Task<Category> GetCategoryByIdAsync(string id)
         {
-            return await _httpClient.GetFromJsonAsync<Category>($"*");
+            return await _httpClient.GetFromJsonAsync<Category>($"{_apiBaseUrl}/{id}");
         }
     }
+
 }

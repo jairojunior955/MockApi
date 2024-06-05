4b64b5047ef5420213d585c3e6041278af646cff
jairojunior955 <jairojunior955@gmail.com>
2024-06-05 15:54:38 -0400

    feat(Category): GetAllCategories

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -5,13 +5,20 @@ using MockApi.Models;
 
 namespace MockApi.Services
 {
-    public class CategoryService()
+    public class CategoryService
     {
         private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl;
 
         public CategoryService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+            _apiBaseUrl = Environment.GetEnvironmentVariable("API_BASE_URL");
+        }
+
+        public async Task<IEnumerable<Category>>GetAllCategoriesAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Category>>(_apiBaseUrl);
         }
     }   
 }

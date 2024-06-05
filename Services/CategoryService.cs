77859f27e35ebc77bfc590acb6a7ad9b8d01f05d
jairojunior955 <jairojunior955@gmail.com>
2024-06-05 16:57:54 -0400

    refactor: API URl

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -8,22 +8,20 @@ namespace MockApi.Services
     public class CategoryService
     {
         private readonly HttpClient _httpClient;
-        private readonly string _apiBaseUrl;
 
         public CategoryService(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            _apiBaseUrl = Environment.GetEnvironmentVariable("API_BASE_URL");
         }
 
-        public async Task<IEnumerable<Category>>GetAllCategoriesAsync()
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
         {
-            return await _httpClient.GetFromJsonAsync<IEnumerable<Category>>(_apiBaseUrl);
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Category>>("*");
         }
 
         public async Task<Category> GetCategoryByIdAsync(string id)
         {
-            return await _httpClient.GetFromJsonAsync<Category>(_apiBaseUrl);
+            return await _httpClient.GetFromJsonAsync<Category>($"*");
         }
-    }   
+    }
 }
